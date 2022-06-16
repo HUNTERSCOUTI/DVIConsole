b12f77fe8912c59777b8f908d5a4e2f4a2f20900
Zilas Jørgensen <Zilas2004@gmail.com>
2022-06-16 17:14:13 +0200

    Update #2

--- DVIConsole/Program.cs
+++ DVIConsole/Program.cs
@@ -1,42 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using Timer = System.Timers.Timer;
 
 namespace DVIConsole
 {
     class DVIMain
     {
-        /* TO DO LIST:
-                Temp and Humidity
-                Time Update constantly
-         */
-
-
-
         private static DVI dvi = new DVI();
         static void Main(string[] args)
         {
-            Timer t = new Timer(300000); //300.000ms = 5 min
-            t.AutoReset = true;
-            t.Start();
+            Timer t = new Timer(10000); //300.000ms = 5 min
+            t.AutoReset = true; t.Start(); // Timer Settings & Start
+            Timer clock = new Timer(100); //300.000ms = 5 min
+            clock.AutoReset = true; clock.Start(); // Timer Settings & Start
 
             dvi.LayoutDraw();
             Writer();
 
+            t.Elapsed += (s, e) =>
+            {
+                Console.Clear();
+                dvi.LayoutDraw();
+                Writer();
+            };
+
             while (true)
             {
                 dvi.ClockWriter();
-                t.Elapsed += (s, e) =>
-                {
-                    Console.Clear();
-                    dvi.LayoutDraw();
-                    Writer();
-                };
-
                 if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.X) break; // PRESS X TO EXIT
             }
         }
@@ -51,107 +40,92 @@ namespace DVIConsole
 
     public class DVI
     {
-        private readonly DVIService.monitorSoapClient ds = new DVIService.monitorSoapClient();
-        private readonly string[] timeZone = { "Romance Standard Time", "Eastern Standard Time", "Singapore Standard Time" };
-        public int TimeZoneId = 0;
+        //private readonly DVIService.monitorSoapClient ds = new DVIService.monitorSoapClient();
         public void LayoutDraw()
         {
             Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.White;
             for (var r = 0; r < Layout.Length; r++)
             {
                 for (var c = 0; c < Layout[r].Length; c++)
                 {
                     Console.Write(Layout[r][c]);
                 }
-                Console.WriteLine();
+                Console.WriteLine(); //Goes one line down
             }
         }
-
         public void StockWriter()
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            for (var k = 0; k < ds.StockItemsUnderMin().Count; k++)
+            for (var k = 0; k < 4; k++)
             {
                 Console.SetCursorPosition(40, k+5);
-                Console.Write(ds.StockItemsUnderMin()[k]);
+                //Console.Write(ds.StockItemsUnderMin()[k]);
+                Console.Write("TESTTESTTEST");
             }
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            for (var j = 0; j < ds.StockItemsOverMax().Count; j++)
+            for (var j = 0; j < 2; j++)
             {
                 Console.SetCursorPosition(40, j+14);
-                Console.Write(ds.StockItemsOverMax()[j]);
+                //Console.Write(ds.StockItemsOverMax()[j]);
+                Console.Write("TESTTESTTEST");
             }
 
             Console.ForegroundColor = ConsoleColor.Green;
-            for (var i = 0; i < ds.StockItemsMostSold().Count; i++)
+            for (var i = 0; i < 3; i++)
             {
                 Console.SetCursorPosition(40, i+23);
-                Console.Write(ds.StockItemsMostSold()[i]);
+                //Console.Write(ds.StockItemsMostSold()[i]);
+                Console.Write("TESTTESTTEST");
             }
         }
-
         public void TempAndHumWriter()
         {
-            //\u2103 *C
-        }
-
-        public void ClockWriter()
-        {
-            int y = 15;
             Console.ForegroundColor = ConsoleColor.Blue;
-            for (int i = 0; i < 3; i++)
-            {
-                Console.SetCursorPosition(16, y);
-                Console.Write(ClockLoader().ToString());
-                y += 2;
-            }
-        }
 
-        public DateTime ClockLoader() //https://docs.microsoft.com/en-us/dotnet/api/system.timezoneinfo.converttimefromutc?view=net-6.0#examples
+            Console.SetCursorPosition(8, 4);
+            //Console.Write(ds.StockTemp());
+            Console.Write("TESTTESTTEST");
+            Console.SetCursorPosition(8, 5);
+            //Console.Write(ds.StockHumidity());
+            Console.Write("TESTTESTTEST");
+
+            Console.SetCursorPosition(8, 8);
+            //Console.Write(ds.OutdoorTemp());
+            Console.Write("TESTTESTTEST");
+            Console.SetCursorPosition(8, 9);
+            //Console.Write(ds.OutdoorHumidity());
+            Console.Write("TESTTESTTEST");
+        }
+        public void ClockWriter()
         {
             DateTime timeUtc = DateTime.UtcNow;
 
-            try
-            {
-                TimeZoneInfo toTimezone = TimeZoneInfo.FindSystemTimeZoneById(timeZone[TimeZoneId]);
-                
+            var copenhagenTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(timeUtc, "Romance Standard Time");
+            var useast = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(timeUtc, "Eastern Standard Time");
+            var singaporeTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(timeUtc, "Singapore Standard Time");
 
-                DateTime time = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, toTimezone);
+            Console.ForegroundColor = ConsoleColor.Blue;
 
-                TimeZoneId++;
-
-                return time;
-            }
-            catch (TimeZoneNotFoundException)
-            {
-                Console.Write("ERROR(1)");
-            }
-            catch (InvalidTimeZoneException)
-            {
-                Console.WriteLine("ERROR(2)");
-            }
-            return timeUtc;
+            Console.SetCursorPosition(16, 15);
+            Console.Write(copenhagenTime.ToString()); // KØBENHAVN
+            Console.SetCursorPosition(16, 17);
+            Console.Write(useast.ToString()); // USA ØST
+            Console.SetCursorPosition(16, 19);
+            Console.Write(singaporeTime.ToString()); // SINGAPORE
         }
-
-        /*
-        //Temp og Fugt
-        ds.StockTemp();
-        ds.OutdoorTemp();
-        ds.OutdoorHumidity();
-        ds.StockHumidity();
-        */
 
         public static readonly char[][] Layout =
         {
             "                                      |                                   ".ToCharArray(), //74 wide
             "      Temperatur og Fugtighed         |         Lagerstatus               ".ToCharArray(),
             "                                      |                                   ".ToCharArray(),
-            "  Lager:                              | Varer under under minimum:        ".ToCharArray(),
+            "  Lager                               | Varer under under minimum:        ".ToCharArray(),
             "  Temp:                               |-----------------------------------".ToCharArray(),
             "  Fugt:                               |                                   ".ToCharArray(),
             "                                      |                                   ".ToCharArray(),
-            "  Udenfor:                            |                                   ".ToCharArray(),
+            "  Udenfor                             |                                   ".ToCharArray(),
             "  Temp:                               |                                   ".ToCharArray(),
             "  Fugt:                               |                                   ".ToCharArray(),
             "                                      |                                   ".ToCharArray(),
@@ -174,7 +148,6 @@ namespace DVIConsole
             "            ---------------           |                                   ".ToCharArray(),
             "           |Press x to exit|          |                                   ".ToCharArray(),
             "            ---------------           |                                   ".ToCharArray() //30 long
-
         };
     }
 }
